ca213d493684fa2ac5aa9ac32ef18b8a0793b197
Diego Cortes <diegocorf123@gmail.com>
2025-03-26 23:08:42 -0600

    feat: add resizeDIBSection

--- code/win64_handmade.cpp
+++ code/win64_handmade.cpp
@@ -6,6 +6,36 @@
 //TODO(diego): this is a global for now
 global_variable bool Running;
 
+global_variable BITMAPINFO BitmapInfo;
+global_variable void *BitmapMemory;
+global_variable HBITMAP BitmapHandle;
+global_variable HDC BitmapDeviceContext;
+
+internal void Win32ResizeDIBSection(int Width, int Height) {
+
+    if(BitmapHandle) {
+        DeleteObject(BitmapHandle);
+    }
+
+    if(!BitmapDeviceContext) {
+        BitmapDeviceContext = CreateCompatibleDC(0);
+    }
+
+    BitmapInfo.bmiHeader.biSize = sizeof(BitmapInfo.bmiHeader);
+    BitmapInfo.bmiHeader.biWidth = Width;
+    BitmapInfo.bmiHeader.biHeight = Height;
+    BitmapInfo.bmiHeader.biPlanes = 1;
+    BitmapInfo.bmiHeader.biBitCount = 32;
+    BitmapInfo.bmiHeader.biCompression = BI_RGB;
+    // TODO: Bulletproof 
+
+    BitmapHandle = CreateDIBSection(BitmapDeviceContext, 
+        &BitmapInfo, 
+        DIB_RGB_COLORS, 
+        &BitmapMemory,
+        0, 0);
+}
+
 internal void Win32UpdateWindow(HDC DeviceContext, int X, int Y, int Width, int Height){
     StretchDIBits(DeviceContext,
         X, Y, Width, Height, 
@@ -26,7 +56,11 @@ LRESULT CALLBACK Win32MainWindowCallback(
 
     switch(Message) {
         case WM_SIZE:{
-            OutputDebugStringA("WM_SIZE\n");
+            RECT ClientRect;
+            GetClientRect(Window, &ClientRect);
+            int Width = ClientRect.right - ClientRect.left;
+            int Height = ClientRect.bottom - ClientRect.top;
+            Win32ResizeDIBSection(Width, Height);
         } break;
         case WM_DESTROY: {
             //TODO(diego): Handle this with a message to the user
